d18b4960a65bd56b9ab4a4e2b0fd5843d6e43759
trox115 <tofernandes67@gmail.com>
2020-07-20 12:42:03 +0100

    initial config

---
+++ spec/requests/inscricao_request_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+RSpec.describe "Inscricaos", type: :request do
+
+end
---
+++ spec/views/nome/Municipio.html.erb_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+RSpec.describe "nome/Municipio.html.erb", type: :view do
+  pending "add some examples to (or delete) #{__FILE__}"
+end
